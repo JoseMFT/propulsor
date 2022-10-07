6bd8a918ec9c0380c53a7fbcb41d6a4f1f0a9f70
JoseMFT <jomaft03@gmail.com>
2022-10-07 13:49:28 +0100

    07/10/2022 - 13:48
    No está terminado, tampoco es para entregar. Me gustaría poder terminarlo.
    A arreglar:
    - Música de juego
    - Música de perder
    - Música de victoria
    - Pantalla de victoria
    - etc.

--- Propulsor/Assets/Scripts/PlayerControler.cs
+++ Propulsor/Assets/Scripts/PlayerControler.cs
@@ -8,32 +8,59 @@ public class PlayerControler: MonoBehaviour {
 
     //float thrust = 3f;
     float combustible = 100f;
+    float sePuedeMover = 1f;
+    float tiempoMensaje = 1f;
+    bool Play = true;
+    bool tocaGasolina = false;
     Rigidbody2D body;
     Vector2 direction;
-    [SerializeField]
-    GameObject prefabParticles;
+    AudioSource Fuel;
+
     [SerializeField]
     float impulse = 15f;
     [SerializeField]
+    GameObject prefabParticles;
+    [SerializeField]
     TextMeshProUGUI FuelLeft;
-    float sePuedeMover = 1f;
+    [SerializeField]
+    GameObject MoreFuel;
+    [SerializeField]
+    GameObject LittleFuel;
     [SerializeField]
     GameObject finalDePartida;
 
     void Start () {
         body = GetComponent<Rigidbody2D> ();
+        Fuel = GetComponent<AudioSource> ();
     }
 
     void Update () {
-        combustible = combustible - Time.deltaTime * 0.5f;
+        if (tocaGasolina == true) {
+            if (tiempoMensaje > 0f) {
+                tiempoMensaje = tiempoMensaje - Time.deltaTime;
+                Debug.Log (tiempoMensaje);
+                if (tiempoMensaje < 0f) {
+                    MoreFuel.SetActive (false);
+                    tocaGasolina = false;
+                }
+            }
+        }
+
+        combustible = combustible - Time.deltaTime * 1.75f;
         if (combustible > 100f) {
             combustible = 100.0f;
         }
         if (combustible <= 100f) {
             FuelLeft.text = combustible.ToString ("00.0") + "%";
             direction.x = Input.GetAxis ("Horizontal") * Time.deltaTime * impulse * sePuedeMover;
-            direction.y = Input.GetAxis ("Vertical") * Time.deltaTime * impulse * sePuedeMover;
+            direction.y = Input.GetAxis ("Vertical") * Time.deltaTime * impulse * sePuedeMover * 0.7f;
         }
+        if (combustible < 30f) {
+            LittleFuel.SetActive (true);
+        } else {
+            LittleFuel.SetActive (false);
+        }
+
         if (combustible < 0f) {
             sePuedeMover = 0f;
             finalDePartida.SetActive (true);
@@ -44,11 +71,14 @@ public class PlayerControler: MonoBehaviour {
         if (choque.tag == "Fuel") {
             combustible = combustible + 15f;
             Debug.Log ("+15 Fuel");
+            Fuel.Play ();
+            MoreFuel.SetActive (true);
+            tocaGasolina = true;
             sePuedeMover = 1f;
+            choque.GetComponent<AudioSource> ().Play ();
+            Instantiate (prefabParticles, choque.transform.position, choque.transform.rotation);
+            Destroy (choque.gameObject, 0.2f);
         }
-
-        Instantiate (prefabParticles, choque.transform.position, choque.transform.rotation);
-        Destroy (choque.gameObject);
     }
 
     private void FixedUpdate () {
